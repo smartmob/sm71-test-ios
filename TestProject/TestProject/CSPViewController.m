b4253ced052b977b5349838cfc2646ca8af58372
Richard Köster <rhm.koster@gmail.com>
2013-11-18 10:10:28 +0100

    add foobar functionality

--- TestProject/TestProject/CSPViewController.m
+++ TestProject/TestProject/CSPViewController.m
@@ -1,11 +1,3 @@
-//
-//  CSPViewController.m
-//  TestProject
-//
-//  Created by Richard Köster on 18/11/2013.
-//  Copyright (c) 2013 Codesoup. All rights reserved.
-//
-
 #import "CSPViewController.h"
 
 @interface CSPViewController ()
@@ -18,12 +10,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self foo:@"bar"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)foo:(NSString *)bar {
+    NSLog(@"%@", bar);
 }
 
 @end
